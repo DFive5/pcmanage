676b67fb24b18d47a0437e263e898f91edaab38d
dz <2371623267@qq.com>
2020-05-22 20:24:49 +0800

    用户信息

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import './plugin/element'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+}, error => {
+// Do something with request error
+  return Promise.reject(error)
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
